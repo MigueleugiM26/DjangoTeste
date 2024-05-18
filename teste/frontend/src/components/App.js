77d63195ede0c737f33aa14315487930d80f0a5a
MigueleugiM26 <129754678+MigueleugiM26@users.noreply.github.com>
2024-05-18 16:27:57 -0300

    v1.0.2

--- teste/frontend/src/components/App.js
+++ teste/frontend/src/components/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
-import{ render } from "react-dom";
-
+import { render } from "react-dom";
 export default class App extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return (<h1>Teste React</h1>);
+    return <h1>Teste React</h1>;
   }
 }
 
