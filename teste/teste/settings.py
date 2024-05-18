1c318f92aa3a8dc1a9ecf6a8efab5f8445f8e38f
MigueleugiM26 <129754678+MigueleugiM26@users.noreply.github.com>
2024-05-18 15:34:04 -0300

    v1.0.2

--- teste/teste/settings.py
+++ teste/teste/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nbp1^py3w=^70kptv7#b$fwrz0g7b&i)(1v*$0o!4bpempn!iy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'rest_framework',
-    'example'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
